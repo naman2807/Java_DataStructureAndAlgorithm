4b49bde2054ad3fcd91dc51208b350db7bbcebc7
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-04 19:45:43 +0530

    Created a new List interface.

---
+++ src/java/util/list/List.java
@@ -0,0 +1,12 @@
+package java.util.list;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: java.util.list
+ * Project Name: Java_DataStructureAndAlgorithm
+ * Date: 04-06-2021
+ */
+
+public interface List {
+}
