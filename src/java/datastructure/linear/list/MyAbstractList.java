dbfd97610291519830dfa9ceb8f884eff2a08f6b
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-05 11:09:20 +0530

    Implemented all interface methods.

--- src/java/datastructure/linear/list/MyAbstractList.java
+++ src/java/datastructure/linear/list/MyAbstractList.java
@@ -1,5 +1,7 @@
 package java.datastructure.linear.list;
 
+import java.util.Collection;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -9,4 +11,78 @@ package java.datastructure.linear.list;
  */
 
 public class MyAbstractList<E> implements MyList<E> {
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    @Override
+    public boolean add(int index, E item) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object item) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public E set(int index, E item) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
