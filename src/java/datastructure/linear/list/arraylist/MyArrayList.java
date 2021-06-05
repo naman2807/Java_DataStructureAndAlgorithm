a8f2ff296dfbe9d96927716069f18d9ae873b50e
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-05 11:34:42 +0530

    Implemented all MyAbstractList.java class methods

--- src/java/datastructure/linear/list/arraylist/MyArrayList.java
+++ src/java/datastructure/linear/list/arraylist/MyArrayList.java
@@ -1,6 +1,7 @@
 package java.datastructure.linear.list.arraylist;
 
 import java.datastructure.linear.list.MyAbstractList;
+import java.util.Collection;
 
 /**
  * Created By: Naman Agarwal
@@ -11,4 +12,79 @@ import java.datastructure.linear.list.MyAbstractList;
  */
 
 public class MyArrayList<E> extends MyAbstractList<E> {
+
+    @Override
+    public boolean add(E item) {
+        return super.add(item);
+    }
+
+    @Override
+    public boolean add(int index, E item) {
+        return super.add(index, item);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return super.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return super.addAll(index, c);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object item) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public E set(int index, E item) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
