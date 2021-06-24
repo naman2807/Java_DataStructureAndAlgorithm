ad66918334dc2856adaa20eab1cc21d6b9701af1
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 15:06:23 +0530

    print the values in set

--- src/practical/Experiment1.java
+++ src/practical/Experiment1.java
@@ -26,9 +26,10 @@ public class Experiment1 {
         for (int i = 0; i < array.length; i++) {
             for(int j = i + 1; j<array.length; j++){
                 if(array[i] == array[j]){
-                    System.out.println(array[j]);
+                    duplicate.add(array[j]);
                 }
             }
         }
+        duplicate.forEach(System.out::println);
     }
 }
