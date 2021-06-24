9665c492e0d604cc5a6d51233b4f096f66b6e531
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 18:12:33 +0530

    Created and implemented Node class

--- src/practical/Experiment10.java
+++ src/practical/Experiment10.java
@@ -1,5 +1,7 @@
 package practical;
 
+import java.util.Stack;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -9,4 +11,30 @@ package practical;
  */
 
 public class Experiment10 {
+
+}
+class Node {
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
