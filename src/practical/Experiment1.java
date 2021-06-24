975ac6badf9637a36796fc8178f095e4b909c874
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 15:03:26 +0530

    Check the condition and print number.

--- src/practical/Experiment1.java
+++ src/practical/Experiment1.java
@@ -22,7 +22,9 @@ public class Experiment1 {
         }
         for (int i = 0; i < array.length; i++) {
             for(int j = i + 1; j<array.length; j++){
-
+                if(array[i] == array[j]){
+                    System.out.println(array[j]);
+                }
             }
         }
     }
