8dc1991dc35465dea6dd323f7359ddc8f45f813a
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 17:49:14 +0530

    Implemented the QueueNode class

--- src/practical/Experiment8.java
+++ src/practical/Experiment8.java
@@ -14,5 +14,25 @@ class QueueNode<E> {
     private E data;
     private Node<E> next;
 
+    public QueueNode(E data) {
+        this.data = data;
+        next = null;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 
 }
