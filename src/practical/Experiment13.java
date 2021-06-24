f21740cd3f6c17ed6ec1f752c7582bff0df55cf2
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-24 18:26:13 +0530

    Implemented Experiment13.java class

--- src/practical/Experiment13.java
+++ src/practical/Experiment13.java
@@ -1,5 +1,7 @@
 package practical;
 
+import java.util.Arrays;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -9,4 +11,33 @@ package practical;
  */
 
 public class Experiment13 {
+    public static void main(String[] args) {
+        int[] arr = {12,8,16,2,5};
+        System.out.println("Unsorted array is ---> "+
+                Arrays.toString(arr));
+        Experiment13 obj = new Experiment13();
+        obj.insertionSort(arr);
+        System.out.println("Sorted array is ---> "+ Arrays.toString(arr));
+    }
+    public void insertionSort(int[] arr){
+        int sortedIndex = 0;
+        int unsortedIndex;
+        for (unsortedIndex = 1; unsortedIndex < arr.length;
+             unsortedIndex++) {
+            int nextAvailableElement = arr[unsortedIndex];
+            for (int j = unsortedIndex; j > 0; j--) {
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    System.out.println("After swapping ");
+                    System.out.println(Arrays.toString(arr));
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+
 }
