debe0085bc75e832161d098ae34ab4d9b20c83f4
naman2807 <namanagarwaldbs438@gmail.com>
2021-09-27 21:44:57 +0530

    Implemented MySinglyLinkedList.java

--- src/linkedlist/singlylinkedlist/MySinglyLinkedList.java
+++ src/linkedlist/singlylinkedlist/MySinglyLinkedList.java
@@ -10,18 +10,79 @@ package linkedlist.singlylinkedlist;
 
 public class MySinglyLinkedList<T> {
     private Node<T> head;
+    private int size = 0;
 
+    // Time Complexity: O(1)
     public void addToFront(T value){
         Node<T> node = new Node<>(value);
         node.setNext(head);
         head = node;
+        size++;
     }
 
+    // Time Complexity: O(1)
     public Node<T> removeFromFront(){
+        if(head == null){
+            return null;
+        }
         Node<T> removedValue = head;
         head = removedValue.getNext();
         removedValue.setNext(null);
+        size--;
         return removedValue;
     }
 
+    public int getSize(){
+        return size;
+    }
+
+    // Time Complexity: O(1)
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    // Time Complexity: O(n)
+    public void printList(){
+        Node<T> current = head;
+        while (head != null){
+            System.out.print(head.getValue() + " -> ");
+            current = current.getNext();
+        }
+
+        System.out.println("null");
+    }
+
+    // Time Complexity: O(n)
+    public void addToLast(T value){
+        Node<T> node = new Node<>(value);
+        if(head == null){
+            head = node;
+            head.setNext(null);
+            return;
+        }
+
+        Node<T> current = head;
+        while (current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(node);
+        node.setNext(null);
+        size++;
+    }
+
+
+    // Time Complexity: O(n)
+    public Node<T> removeFromLast(){
+        if(head == null){
+            return null;
+        }
+        Node<T> previousValue = head;
+       for(int i = 0; i < size - 1; i++){
+           previousValue = previousValue.getNext();
+       }
+        Node<T> removedValue = previousValue.getNext();
+        previousValue.setNext(null);
+        size--;
+        return removedValue;
+    }
 }
