77170b8a96da08437bbfbd84ee69a118fee87cce
naman2807 <namanagarwaldbs438@gmail.com>
2021-09-27 21:21:06 +0530

    Created Main.java calss in singlylinkedlist package

---
+++ src/linkedlist/singlylinkedlist/Main.java
@@ -0,0 +1,12 @@
+package linkedlist.singlylinkedlist;
+
+/**
+ * Created By: Naman Agarwal
+ * User ID: naman2807
+ * Package Name: linkedlist.singlylinkedlist
+ * Project Name: Java_DataStructureAndAlgorithm
+ * Date: 27-09-2021
+ */
+
+public class Main {
+}
