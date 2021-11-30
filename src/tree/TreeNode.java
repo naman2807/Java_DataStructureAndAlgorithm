baa21a5a18ced29fbea1eba5f89874402d7c5d88
naman2807 <namanagarwaldbs438@gmail.com>
2021-11-30 10:13:47 +0530

    Implemented TreeNode.java

--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -1,5 +1,7 @@
 package tree;
 
+import com.sun.source.tree.Tree;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -9,5 +11,37 @@ package tree;
  */
 
 public class TreeNode<T> {
+    private TreeNode<T> previous;
+    private TreeNode<T> next;
+    private T data;
 
+    public TreeNode(TreeNode<T> previous, TreeNode<T> next, T data) {
+        this.previous = previous;
+        this.next = next;
+        this.data = data;
+    }
+
+    public TreeNode<T> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(TreeNode<T> previous) {
+        this.previous = previous;
+    }
+
+    public TreeNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(TreeNode<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
