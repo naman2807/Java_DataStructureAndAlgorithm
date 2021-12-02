7a296cb05ee3ff0243f58d05a475eff0f37cf9d3
naman2807 <namanagarwaldbs438@gmail.com>
2021-12-03 00:49:47 +0530

    Implemented the traversal method

--- src/linkedlist/circularlinkedlist/MyCircularSinglyLinkedList.java
+++ src/linkedlist/circularlinkedlist/MyCircularSinglyLinkedList.java
@@ -1,5 +1,7 @@
 package linkedlist.circularlinkedlist;
 
+import linkedlist.singlylinkedlist.Node;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -8,5 +10,47 @@ package linkedlist.circularlinkedlist;
  * Date: 03-12-2021
  */
 
-public class MyCircularSinglyLinkedList {
+public class MyCircularSinglyLinkedList<T> {
+    private Node<T> last;
+    private int length;
+
+    public MyCircularSinglyLinkedList() {
+        this.last = null;
+        this.length = 0;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    // Brute Force Method
+    public void createCircularLinkedList(){
+        Node<Integer> first = new Node<>(1);
+        Node<Integer> second = new Node<>(2);
+        Node<Integer> third = new Node<>(3);
+        Node<Integer> fourth = new Node<>(4);
+
+        first.setNext(second);
+        second.setNext(third);
+        third.setNext(fourth);
+        fourth.setNext(first);
+    }
+
+    // Traversal
+    public void display(){
+        if (last == null){
+            return;
+        }
+
+        Node<T> first = last.getNext();
+        while (first != last){
+            System.out.print(first.getValue() + " ");
+            first = first.getNext();
+        }
+        System.out.println(first.getValue());
+    }
 }
