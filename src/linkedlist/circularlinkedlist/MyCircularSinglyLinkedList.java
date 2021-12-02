cb8582e81781609ef00be6f6d3e7f9025893ca14
naman2807 <namanagarwaldbs438@gmail.com>
2021-12-03 00:57:27 +0530

    Implemented the deleteFirst() method

--- src/linkedlist/circularlinkedlist/MyCircularSinglyLinkedList.java
+++ src/linkedlist/circularlinkedlist/MyCircularSinglyLinkedList.java
@@ -2,6 +2,8 @@ package linkedlist.circularlinkedlist;
 
 import linkedlist.singlylinkedlist.Node;
 
+import java.util.NoSuchElementException;
+
 /**
  * Created By: Naman Agarwal
  * User ID: naman2807
@@ -19,16 +21,16 @@ public class MyCircularSinglyLinkedList<T> {
         this.length = 0;
     }
 
-    public int getLength(){
+    public int getLength() {
         return length;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return length == 0;
     }
 
     // Brute Force Method
-    public void createCircularLinkedList(){
+    public void createCircularLinkedList() {
         Node<Integer> first = new Node<>(1);
         Node<Integer> second = new Node<>(2);
         Node<Integer> third = new Node<>(3);
@@ -41,13 +43,13 @@ public class MyCircularSinglyLinkedList<T> {
     }
 
     // Traversal
-    public void display(){
-        if (last == null){
+    public void display() {
+        if (last == null) {
             return;
         }
 
         Node<T> first = last.getNext();
-        while (first != last){
+        while (first != last) {
             System.out.print(first.getValue() + " ");
             first = first.getNext();
         }
@@ -55,11 +57,11 @@ public class MyCircularSinglyLinkedList<T> {
     }
 
     // Insert the node at the start of circular linked list
-    public void insertFirst(T data){
+    public void insertFirst(T data) {
         Node<T> temp = new Node<>(data);
-        if (last == null){
+        if (last == null) {
             last = temp;
-        }else {
+        } else {
             temp.setNext(last.getNext());
         }
         last.setNext(temp);
@@ -67,12 +69,12 @@ public class MyCircularSinglyLinkedList<T> {
     }
 
     // Insert the node at the end of circular linked list
-    public void insertLast(T data){
+    public void insertLast(T data) {
         Node<T> temp = new Node<>(data);
-        if (last == null){
+        if (last == null) {
             last = temp;
             last.setNext(temp);
-        }else {
+        } else {
             temp.setNext(last.getNext());
             last.setNext(temp);
             last = temp;
@@ -80,5 +82,19 @@ public class MyCircularSinglyLinkedList<T> {
         length++;
     }
 
-
+    // Remove the first node from circular linked list
+    public Node<T> deleteFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Circular Linked List is empty!");
+        }
+        Node<T> temp = last.getNext();
+        if (last.getNext() == last) {
+            last = null;
+        } else {
+            last.setNext(temp.getNext());
+        }
+        temp.setNext(null);
+        length--;
+        return temp;
+    }
 }
